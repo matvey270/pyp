af059bb279c049a4491d4e2d3463015bdf0b5594
matvey270 <127657591+matvey270@users.noreply.github.com>
2023-11-21 22:28:07 +0300

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,9 +1,12 @@
-def f(a):
-    for x in range (1000):
-        if not(int(x%33)==0 <= (int(x%45!=0) <= int(x%a!=0))):
-            return False
-        return True
-a=1 
-while not f(a):
-    a+=1
-print(a)
+with open('17-3.txt') as l:
+    a=[int(i) for i in l]
+n=len(a)
+cnt=0
+mx=-10**9
+
+for i in range(n-1):
+    x,y=a[i],a[i+1]
+    if x%2==0 and x%4==0 and y%2!=0 and y%11==0 or y%2==0 and y%4==0 and x%2!=0 and x%11==0:
+        cnt+=1
+        mx=max(mx, x+y)
+print(cnt, mx)
