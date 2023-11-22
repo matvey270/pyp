dddbc324e0210f594a61e64ab6144c2bc86d9d06
matvey270 <127657591+matvey270@users.noreply.github.com>
2023-11-22 19:56:10 +0300

    Add files via upload

---
+++ 195pyp.py
@@ -0,0 +1,31 @@
+with open('17-9.txt') as l:
+    a=[int(i) for i in l]
+n=len(a)
+cnt=0
+mx=[]
+
+def f(x):
+    x=bin(x)
+    po=0
+    pl=0
+    for i in x:
+        if i == '0':
+            po+=1
+        else:
+            pl+=1
+    if po>=1 and pl>=3:
+        return True
+    else:
+        return False
+
+
+for i in range(2,n):
+    x,y,z=a[i-2],a[i-1], a[i]
+    if f(x)==True and f(y)==True or f(z)==True and f(y)==True or f(x)==True and f(z)==True :
+        print('Yes')
+        cnt+=1
+        b=max(x,y,z)
+
+        mx.append(b)
+ans=sum(mx)
+print(cnt, ans)
